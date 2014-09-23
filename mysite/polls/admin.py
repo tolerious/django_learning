36bdb1f00479838d7c5cac92e8733fefb13ac708
fengtianba <fengtianbanjit@gmail.com>
2014-09-23 20:55:04 +0800

    reverse the location of the change question text field and the data published field

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from polls.models import Question
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_data', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
