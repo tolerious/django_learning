6bd4f09f2a158d39a040304770a1e0d2607a9f00
fengtianba <fengtianbanjit@gmail.com>
2014-09-24 11:07:19 +0800

    show three column on Question page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from polls.models import Choice,Question
 
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
 	model = Choice
 	extra = 3
 class QuestionAdmin(admin.ModelAdmin):
@@ -11,4 +11,5 @@ class QuestionAdmin(admin.ModelAdmin):
 	    ('Date information',	{'fields':['pub_data'], 'classes':['collapse']}),
 	]
 	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_data', 'was_published_recently')
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,7 @@ class Question(models.Model):
 	def __unicode__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
