6b77321148142d75fc3468c66f83c1d722cb8bb1
fengtianba <fengtianbanjit@gmail.com>
2014-09-24 11:21:45 +0800

    add filter field and search fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,4 +12,6 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 	inlines = [ChoiceInline]
 	list_display = ('question_text', 'pub_data', 'was_published_recently')
+	list_filter = ['pub_data']
+	search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
