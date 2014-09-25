312f3f7ca03f6b0a9ca262d0fef36a4fe0cfde0c
fengxiaolong <fengtianbanjit@gmail.com>
2014-09-25 22:17:10 +0800

    reformate code and create index.html in polls templates directory

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,17 +4,20 @@ from django.http import HttpResponse
 from polls.models import Question
 # Create your views here.
 def index(request):
-	#return HttpResponse("Hello, world. You're at the polls index.")
-	latest_question_list = Question.objects.order_by('-pub_data')[:5]
-	output = ', '.join([p.question_text for p in latest_question_list])
-	return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_data')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question %s." % question_id)
