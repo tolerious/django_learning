5c0b991c69935e562dde8f9fcbe8223f5ec01656
tolerious <toleriousnjit@gmail.com>
2014-09-26 12:07:20 +0800

    Raising a 404 error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
 
 from polls.models import Question
 from django.template import RequestContext, loader
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by('-pub_data')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.all().order_by('-pub_data')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
